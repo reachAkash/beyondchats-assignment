9ac53217c146b841402ab4a99a254c36938b2860
reachAkash <akashgupta9062@gmail.com>
2025-12-31 00:20:14 +0530

    update : error handling & loader added.

--- client/src/api/articles.ts
+++ client/src/api/articles.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE =
-  `${import.meta.env.VITE_API_BASE_URL}/api/articles` ||
+  (import.meta.env.VITE_API_BASE_URL &&
+    `${import.meta.env.VITE_API_BASE_URL}/api/articles`) ||
   "http://localhost:8000/api/articles";
 
 export const getArticles = () => axios.get(API_BASE);
--- client/src/pages/ArticlesList.tsx
+++ client/src/pages/ArticlesList.tsx
@@ -5,28 +5,54 @@ import type { Article } from "../types/articles";
 
 export default function ArticlesList() {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    getArticles().then((res) => {
-      setArticles(res.data);
-      console.log(res.data);
-    });
+    getArticles()
+      .then((res) => {
+        setArticles(res.data);
+      })
+      .catch(() => {
+        setError("Failed to load articles");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return (
+      <div className="container">
+        <h1>Articles</h1>
+        <div className="loader">Fetching articles...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="container">
+        <h1>Articles</h1>
+        <p className="error">{error}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
       <h1>Articles</h1>
 
-      {articles.map((a) => (
-        <div key={a._id} className="card">
+      {articles?.map((a) => (
+        <div key={a?._id} className="card">
           <div className="card-header">
-            <h3>{a.title}</h3>
-            <span className={`status ${a.status.toLowerCase()}`}>
-              {a.status}
+            <h3>{a?.title}</h3>
+            <span className={`status ${a?.status.toLowerCase()}`}>
+              {a?.status}
             </span>
           </div>
 
-          <Link className="view-link" to={`/article/${a._id}`}>
+          <Link className="view-link" to={`/article/${a?._id}`}>
             View →
           </Link>
         </div>
